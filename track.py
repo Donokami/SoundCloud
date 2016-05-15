06cb8393799d4a2db1b6073f832b501fffc8ac83
Kiyoshi <kiyoshi896@gmail.com>
2016-05-15 20:27:03 +0200

    Bugfiex and improvements.

--- track.py
+++ track.py
@@ -1,71 +1,60 @@
 # -*- coding: utf-8 -*-
 
-"""Download a single track"""
+import requests, os.path, soundcloud
+from collections import Counter
+import utils, track
 
-import urllib, re, os.path, soundcloud
-import utils
-import eyed3
-import eyed3.id3
 
-def download_from_url(client_id, track_url, dir, override=False):
-    """Download from URL"""
+def download_from_url(client_id, url, base_dir, override=False):
+    """Download the given playlist"""
+    downloaded = 0
+    skipped = 0
+    errors = 0
+    traitement = 0
+
+    # Retrieve playlist data
     client = soundcloud.Client(client_id=client_id)
-    track = client.get('/resolve', url=track_url)
-    download(client, track, dir, False, override)
+    playlist = client.get('/resolve', url=url)
 
-
-def download_from_id(client_id, track_id, dir, playlist=False, override=False):
-    """Download using the song id"""
-    client = soundcloud.Client(client_id=client_id)
-    track = client.get('/tracks/%d' % track_id, allow_redirects=False)
-    download(client, track, dir, playlist, override)
-
-
-def download(client, track, dir, playlist=False, override=False):
-    """Download a track using the given SC client"""
-    title = fix_title(track.title, track.user['username'])
-    print '"%s"' % title
-    if not dir: dir = 'mp3'
+    # Create dir
+    playlist_title = playlist.title
+    dir = os.path.join(base_dir, playlist_title)
     utils.create_dir(dir)
-    file_name = utils.build_file_name(dir, title)
 
-    if not override and os.path.exists(file_name):
-        print "File already exists, skipped"
-        return False
+    # Download tracks
+    for trak in playlist.tracks:
+        try:
+            #done = song.down(client, track, dir, override)
+            done = track.download_from_id(client_id, trak['id'], dir, playlist, override)
+            if done == True:
+                downloaded = downloaded + 1
+            elif done == False:
+                skipped = skipped + 1
+            elif done == "Error":
+                errors = errors + 1
+        except requests.exceptions.HTTPError, err:
+            if err.response.status_code == 404:
+                print 'Error: could not download'
+                errors = errors + 1
+            else:
+                raise
 
-    stream_url = client.get(track.stream_url, allow_redirects=False)
-    urllib.urlretrieve(stream_url.location, file_name)
-    if track.artwork_url is None:
-        track.artwork_url = track.user['avatar_url']
-    track.artwork_url = track.artwork_url.replace("-large.", "-t500x500.", 1)
-    track.artwork_url = track.artwork_url.replace("https:", "http:", 1)
-    imagedata = urllib.urlopen(track.artwork_url).read()
-    audiofile = eyed3.load(file_name)
-    audiofile.tag = eyed3.id3.Tag()
-    audiofile.tag.file_info = eyed3.id3.FileInfo(file_name)
-    audiofile.tag.images.set(3,imagedata,"image/jpeg")
-    audiofile.tag.artist = u"%s" % track.user['username']
-    if playlist == False:
-        audiofile.tag.album = u"%s" % u"Everything"
-        audiofile.tag.album_artist = u"%s" % "Various Artists"
-    else:
-        audiofile.tag.album = u"%s" % playlist.title
-        audiofile.tag.album_artist = u"%s" % playlist.user['username']
-    audiofile.tag.title = u"%s" % track.title
-    audiofile.tag.save()
-    return True
+    print 'Playlist downloaded to "%s"' % playlist_title
+    return Counter({
+        'downloaded': downloaded, 'skipped': skipped, 'errors': errors,
+    })
 
 
-def fix_title(title, user_name):
-    """Fix title (missing space, illegal chars, missing author)"""
-    # Add missing dash
-    title = re.sub(r"^(.*\S)- (.*)$", r"\1 - \2", title)
+def download_all(client_id, user_url, base_dir, override=False):
+    """Download all playlist from the given user URL"""
+    client = soundcloud.Client(client_id=client_id)
+    user = client.get('/resolve', url=user_url)
+    playlists = client.get('/users/%d/playlists' % user.id)
+    stats = Counter()
 
-    # Remove adds
-    title = title.split('//', 1)[0]
+    for playlist in playlists:
+        print 'Playlist: "%s"' % playlist.title
+        playlist_stats = download_from_url(client_id, playlist.permalink_url, base_dir, override)
+        stats = stats + playlist_stats
 
-    # Prepend username if author seems to be missing
-    if ' - ' not in title:
-        title = '%s - %s' % (user_name, title)
-
-    return title.strip()
+    return stats
